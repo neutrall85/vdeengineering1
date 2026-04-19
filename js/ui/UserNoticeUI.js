c13d5074d09f462b0ceb256bf1d8076e33604204
neutrall85 <canni85@mail.ru>
2026-04-19 18:00:00 +0400

    fix

--- js/ui/UserNoticeUI.js
+++ js/ui/UserNoticeUI.js
@@ -79,12 +79,8 @@ class UserNoticeUI {
               </button>
             </div>
             <div class="user-notice-links">
-              <a href="#" class="user-privacy-link" id="user-privacy-link">
-                Политика конфиденциальности
-              </a>
-              <a href="#" class="user-privacy-link" id="user-cookie-policy-link">
-                Политика в отношении файлов cookie
-              </a>
+             <a href="#" class="user-privacy-link" id="user-privacy-link">Политика конфиденциальности</a> 
+             <a href="#" class="user-privacy-link" id="user-cookie-policy-link">Политика в отношении файлов cookie</a>
             </div>
           </div>
         </div>
